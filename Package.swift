03ca44c65c4d83c8b287112e41fc3205895226ed
Ding Songwen <DingSoung@gmail.com>
2017-09-20 17:18:30 +0800

    swit package manager and script

--- Package.swift
+++ Package.swift
@@ -1,10 +1,5 @@
-// swift-tools-version:4
-
 import PackageDescription
 
 let package = Package(
     name: "Model-Cache",
-    dependencies: [
-        .Package(url: "https://github.com/DingSoung/Extension.git", "0.6.0")
-    ]
 )
